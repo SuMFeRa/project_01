dc82ce23c11ded0a8ae9feacf4f485a0185df8ae
SuMFeRa <Beserith1121@gmail.com>
2022-05-25 10:52:19 +0300

    change dialog .css

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,12 +2,40 @@ import React from "react";
 import s from "./Dialogs.module.css";
 
 
-
-
-const Dialogs = () => { 
+const Dialogs = () => {
     return (
-        <div className={s.header}>
-
+        <div className={s.dialogs}>
+            <div className={s.dialogsItem}>
+                <div className={s.dialog}>
+                    Dmitry
+                </div>
+                <div className={s.dialog}>
+                    Anna
+                </div>
+                <div className={s.dialog}>
+                    Kiryll
+                </div>
+                <div className={s.dialog}>
+                    Andrey
+                </div>
+                <div className={s.dialog}>
+                    Sveta
+                </div>
+                <div className={s.dialog}>
+                    Sasha
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>
+                    Hi
+                </div>
+                <div className={s.message}>
+                    My friend
+                </div>
+                <div className={s.message}>
+                    Yo
+                </div>
+            </div>
         </div>
     )
 }
