0a8b3d23074e707368e9f472ef8cd4488de9f887
SuMFeRa <Beserith1121@gmail.com>
2022-06-02 08:49:01 +0300

    Dividing a file Dialog into folders DialogsItem and Message

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -6,29 +6,31 @@ import Post from './Posts/Post';
 const MyPosts = () => {
 
 
-let postsData = [
-    {id: 1, message: "hi, friends", likesCount: 12},
-    {id: 2, message: "hi, world", likesCount: 11}
-]
+    let postsData = [
+        {id: 1, message: "hi, friends", likesCount: 12},
+        {id: 2, message: "hi, world", likesCount: 11}
+    ]
 
-return (
-    <div className={s.postsBlock}>
-        <h3>My posts</h3>
-        <div>
+    let postsElement = postsData
+        .map((post) => <Post message={post.message} id={post.id} likesCount={post.likesCount}/>)
+
+    return (
+        <div className={s.postsBlock}>
+            <h3>My posts</h3>
             <div>
-                <textarea name="" id="" cols="40" rows="5"></textarea>
+                <div>
+                    <textarea name="" id="" cols="40" rows="5"></textarea>
+                </div>
+                <div>
+                    <button>Add Post</button>
+                </div>
             </div>
-            <div>
-                <button>Add Post</button>
+
+            <div className={s.posts}>
+                {postsElement}
             </div>
         </div>
-
-        <div className={s.posts}>
-            <Post message={postsData[0].message} id={postsData[0].id} likesCount={postsData[0].likesCount} />
-            <Post message={postsData[1].message} id={postsData[1].id} likesCount={postsData[1 ].likesCount}/>
-        </div>
-    </div>
-)
+    )
 }
 
 export default MyPosts;
